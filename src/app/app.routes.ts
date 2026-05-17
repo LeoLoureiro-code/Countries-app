c9457f37871a14cba3f2761e5d39baeb2c738b68
Leo <leolou.90@gmail.com>
2026-05-16 22:35:02 -0700

    add default and home routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CountriesHome } from '../pages/countries-home/countries-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'home', pathMatch: 'full'},
+    {path: 'home', component: CountriesHome}
+];
