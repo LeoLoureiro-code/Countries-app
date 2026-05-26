068b35f55d7ff28c445ce1ce2cc07d23d6d6d544
Leo <leolou.90@gmail.com>
2026-05-25 17:03:07 -0700

    change url to request more info

--- src/services/countries-service/countries-service.ts
+++ src/services/countries-service/countries-service.ts
@@ -5,18 +5,18 @@ import { Injectable, signal } from '@angular/core';
 })
 export class CountriesService {
   
-  countries = signal<any[]>([]);
+  countries:any = [];
 
   async loadCountries(){
 
     const response = await fetch (
-      'https://restcountries.com/v3.1/all'
+      'https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,capital,tld,currencies,languages,borders'
     );
 
     const data = await response.json();
 
-    this.countries.set(data);
+    this.countries = (data);
 
-
+    return this.countries;
   }
 }
