0f0e85958e55638d72a444f9d101c95c544d4521
Leo <leolou.90@gmail.com>
2026-05-31 17:34:54 -0700

    add new route for error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { CountriesHome } from '../pages/countries-home/countries-home';
+import { CountriesDetails } from '../pages/countries-details/countries-details';
+import {CountriesNotFound} from '../pages/countries-not-found/countries-not-found'
 
 export const routes: Routes = [
     {path: '', redirectTo:'home', pathMatch: 'full'},
-    {path: 'home', component: CountriesHome}
+    {path: 'home', component: CountriesHome},
+    {path: 'details/:id', component: CountriesDetails},
+    {path: '**', component: CountriesNotFound},
 ];
