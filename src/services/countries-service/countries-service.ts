f0d8229fd452a625b543c20393ec9f8592c830f6
Leo <leolou.90@gmail.com>
2026-05-31 17:40:44 -0700

    add method to request info for selected country

--- src/services/countries-service/countries-service.ts
+++ src/services/countries-service/countries-service.ts
@@ -12,7 +12,11 @@ export class CountriesService {
 
   loadCountries(){
 
-    return this.httpClient.get('https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,capital,tld,currencies,languages,borders');
+    return this.httpClient.get('https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,capital,cca3');
   
   } 
+
+  loadSelectedCountry(code:any){
+    return this.httpClient.get<any[]>('https://restcountries.com/v3.1/alpha/'+code);
+  }
 }
